2bd27a740fcbe413502579e2f17390bd7d867469
Nathnael asefa <Nathnaelasefa@gmail.com>
2025-04-13 00:40:14 +0300

    changes in image

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: isProd ? "/PortfolioTraining" : " ",
   output:"export",
   distDir:"dist",
   images: {
